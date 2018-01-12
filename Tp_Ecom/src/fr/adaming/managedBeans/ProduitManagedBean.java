2b9b1bba4d9703e7e5b232975c1d86bed5c63374
intiformation <intiformation@192.168.1.152>
2018-01-12 15:58:29 +0100

    arnaud

--- Tp_Ecom/src/fr/adaming/managedBeans/ProduitManagedBean.java
+++ Tp_Ecom/src/fr/adaming/managedBeans/ProduitManagedBean.java
@@ -1,7 +1,12 @@
 package fr.adaming.managedBeans;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.Serializable;
+import java.net.MalformedURLException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -16,6 +21,20 @@ import javax.servlet.http.HttpSession;
 import org.apache.commons.codec.binary.Base64;
 import org.primefaces.event.FileUploadEvent;
 import org.primefaces.model.UploadedFile;
+
+import com.itextpdf.awt.geom.CubicCurve2D.Float;
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
 
 import fr.adaming.model.Admin;
 import fr.adaming.model.Categorie;
@@ -82,7 +101,7 @@ public class ProduitManagedBean implements Serializable {
 	public void setpService(IProduitService pService) {
 		this.pService = pService;
 	}
-	
+
 	public UploadedFile getFile() {
 		return file;
 	}
@@ -105,8 +124,8 @@ public class ProduitManagedBean implements Serializable {
 		// //Appel de la methode service
 
 		this.produit = pService.addProduit(this.produit);
-		
-		System.out.println("------------------- "+this.produit.getIdProduit() );
+
+		System.out.println("------------------- " + this.produit.getIdProduit());
 		if (this.produit.getIdProduit() != 0) {
 			// Recuperer la nouvelle liste de la bd
 			this.listeProduits = pService.getAllProduit();
@@ -119,14 +138,14 @@ public class ProduitManagedBean implements Serializable {
 			return "ajout_produit";
 		}
 	}
-	
-	public void upload(FileUploadEvent event){
+
+	public void upload(FileUploadEvent event) {
 		UploadedFile ufile = event.getFile();
 		byte[] contents = ufile.getContents();
 		this.produit.setPhoto(contents);
-		this.image = "data:image/png;base64,"+Base64.encodeBase64String(contents);
+		this.image = "data:image/png;base64," + Base64.encodeBase64String(contents);
 	}
-	
+
 	public String updateProduit() {
 		Produit pModif = pService.updateProduit(this.produit);
 		if (pModif != null) {
@@ -151,10 +170,10 @@ public class ProduitManagedBean implements Serializable {
 			FacesContext.getCurrentInstance().addMessage(null,
 					new FacesMessage("Une erreur est survenue lors de la recherche."));
 		}
-		return "recherche_produit";
+		return "rechercher_produit";
 	}
-	
-	public String getProduitbyName(){
+
+	public String getProduitbyName() {
 		Produit pFind = pService.getProduitbyName(this.produit.getDesignation());
 		pFind.setImage("data:image/png;base64," + Base64.encodeBase64String(pFind.getPhoto()));
 		if (pFind != null) {
@@ -163,7 +182,7 @@ public class ProduitManagedBean implements Serializable {
 			FacesContext.getCurrentInstance().addMessage(null,
 					new FacesMessage("Une erreur est survenue lors de la recherche."));
 		}
-		return "affiche_produit";
+		return "rechercher_produit";
 	}
 
 	public String deleteProduit() {
@@ -174,6 +193,7 @@ public class ProduitManagedBean implements Serializable {
 		maSession.setAttribute("produitList", listeP);
 		return "accueilAdmin";
 	}
+
 	public String deleteProduitByName() {
 		pService.deleteProduitByName(this.produit.getDesignation());
 		// Recuperer la nouvelle liste de la bd
@@ -186,14 +206,14 @@ public class ProduitManagedBean implements Serializable {
 	public String getAllProduits() {
 		this.listeProduits = pService.getAllProduit();
 		if (listeProduits.size() > 0) {
-			
+
 			List<Produit> listeTemp = new ArrayList<Produit>();
 			for (Produit prod : listeProduits) {
 				prod.setImage("data:image/png;base64," + Base64.encodeBase64String(prod.getPhoto()));
 				listeTemp.add(prod);
 			}
 			this.setListeProduits(listeTemp);
-			
+
 			maSession.setAttribute("produitList", this.listeProduits);
 		} else {
 			FacesContext.getCurrentInstance().addMessage(null,
@@ -201,4 +221,88 @@ public class ProduitManagedBean implements Serializable {
 		}
 		return "affiche_produits";
 	}
+
+	public void createPdf() throws IOException {
+		Rectangle pageSize = new Rectangle(780, 525);
+		Document document = new Document(pageSize);
+		String chemin = "C:\\Users\\intiformation\\Desktop\\testlistProd.pdf";
+
+		try {
+			PdfWriter.getInstance(document, new FileOutputStream(chemin));
+			document.open();
+			addInfoPage(document);
+			document.add(tableauProduit());
+			// document.add(Paragraph1());
+			Paragraph preface = new Paragraph();
+			preface.setAlignment(Element.ALIGN_CENTER);
+			preface.add(" Ecom Carnaval");
+		} catch (FileNotFoundException e) {
+
+			e.printStackTrace();
+		} catch (DocumentException e) {
+
+			e.printStackTrace();
+		}
+		// Fermeture du document
+		document.close();
+
+	}
+
+	private static void addInfoPage(Document document) throws DocumentException,  IOException {
+		Paragraph preface = new Paragraph();
+		//String Img = "/Tp_Ecom/WebContent/resources/images/carnaval.jpg";
+		//Image image = Image.getInstance(Img);
+		
+		Font catFont = new Font(Font.FontFamily.TIMES_ROMAN, 24, Font.BOLD, BaseColor.RED);
+		Font catFont2 = new Font(Font.FontFamily.TIMES_ROMAN, 20, Font.NORMAL, BaseColor.BLACK);
+		Font redFont = new Font(Font.FontFamily.TIMES_ROMAN, 18, Font.NORMAL, BaseColor.RED);
+		Font subFont = new Font(Font.FontFamily.TIMES_ROMAN, 16, Font.BOLD);
+		Font smallBold = new Font(Font.FontFamily.TIMES_ROMAN, 12, Font.BOLD);
+		
+		addEmptyLine(preface, 1);
+		
+		preface.add(new Paragraph("Ecom Carnaval", catFont));
+		preface.add(new Paragraph("Date : " + new Date(), smallBold));
+		preface.add(new Paragraph("Voici le récapitulatif des produits enregistrés :", catFont2));
+		addEmptyLine(preface, 1);
+		
+		addEmptyLine(preface, 2);
+		document.add(preface);
+	}
+
+	public PdfPTable tableauProduit() {
+		List<Produit> prodtot = pService.getAllProduit();
+		// On créer un objet table dans lequel on intialise sa taille.
+		PdfPTable tableProd = new PdfPTable(3);
+
+		// On créer l'objet cellule.
+		PdfPCell cell;
+
+		cell = new PdfPCell(new Phrase("Information Liste Produit"));
+		cell.setColspan(3);
+		tableProd.addCell(cell);
+
+		// contenu du tableau.
+		for (Produit p : prodtot) {
+			// ajout colonne produit
+			cell = new PdfPCell(new Phrase("Produit " + p.getIdProduit()));
+			cell.setRowspan(2);
+			tableProd.addCell(cell);
+			// ajout colonne specifique au produit
+			tableProd.addCell("Designation " + p.getDesignation());
+			tableProd.addCell("Description " + p.getDescription());
+			tableProd.addCell("Prix " + p.getPrix());
+			tableProd.addCell("Quantité " + p.getQuantite());
+		}
+
+		return tableProd;
+
+	}
+
+	private static void addEmptyLine(Paragraph paragraph, int number) {
+		for (int i = 0; i < number; i++) {
+			paragraph.add(new Paragraph(" "));
+		}
+	}
+
 }
--- Tp_Ecom/src/fr/adaming/test/TestPdf.java
+++ Tp_Ecom/src/fr/adaming/test/TestPdf.java
@@ -1,11 +1,53 @@
 package fr.adaming.test;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
 
 public class TestPdf {
 
-//	public static final String chemin ="C:\\Users\\Documents\\testpdf.pdf";
-//	
-//	public static void main(String[] args) throws DocumentException
+	public static final String chemin ="C:\\Users\\intiformation\\Desktop\\testpdf5.pdf";
+	
+	public static void main(String[] args) throws DocumentException, IOException{
+		
+		Document document = new Document();
+		
+		try {
+			PdfWriter.getInstance(document, new FileOutputStream(chemin));
+			document.open();
+			//ajout du contenu
+			document.add(new Paragraph("EHEH new pdf incoming"));
+			 PdfPTable table = new PdfPTable(8);
+			table.addCell("olalal");
+			table.addCell("ohoh");
+			table.addCell("ohoh");
+			table.addCell("ohoh");
+			table.addCell("ohoh");
+			table.addCell("ohoh");
+			table.addCell("ohoh");
+			table.addCell("ohoh");
+			table.addCell("olalal");
+			table.addCell("ohoh");
+			table.addCell("ohoh");
+			table.addCell("ohoh");
+			table.addCell("ohoh");
+			table.addCell("ohoh");
+			table.addCell("ohoh");
+			table.addCell("ohoh");
+			 document.add(table);
+			
+		} catch (DocumentException de) {
+			de.printStackTrace();
+		}catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+		//Fermeture du document
+		document.close();
+	}
 	
 }
